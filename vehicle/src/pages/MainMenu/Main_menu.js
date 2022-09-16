d2075bbe22809b1b6586b95ce73863d1db3dd5b4
student <you@example.com>
2022-09-16 22:28:32 +0300

    trying to work with api of citys

--- vehicle/src/pages/MainMenu/Main_menu.js
+++ vehicle/src/pages/MainMenu/Main_menu.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Myimage from "../../pictures/Logo_Hatzerim.jpg";
 import { useParams, Link, Outlet, useLocation } from "react-router-dom";
 import "./Main_menu.scss";
 export default function MainMenu() {
   let { id } = useParams();
   const { search } = useLocation();
-
   const Da = new Date();
+  const [date, setDate] = useState(Da);
+
+  useEffect(() => {
+    const Da = new Date();
+    setDate(Da.toLocaleString());
+  }, [date]);
+
+  setInterval(function () {
+    let da = new Date();
+    setDate(da.toLocaleString());
+  }, 1000);
 
   return (
     <>
       <div className="MainMenuDiv">
         <div className="header">
           <img src={Myimage} alt="Logo"></img>
-          <div>{Da.toLocaleString()}</div>
+          <div>{date.toLocaleString()}</div>
           <label>Hello </label>
         </div>
         <nav className="topnav">
