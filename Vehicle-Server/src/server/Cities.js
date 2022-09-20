4133f8b51bab84890258b92d699a567c3e3da361
student <you@example.com>
2022-09-20 17:29:15 +0300

    added cities of israel api

---
+++ Vehicle-Server/src/server/Cities.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+axios
+  .get(
+    "https://data.gov.il/api/3/action/datastore_search?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=10000"
+  )
+  .then(function (response) {
+    // handle success
+    console.log(response.data.result);
+  })
+  .catch(function (error) {
+    console.log("handle error");
+    console.log(error);
+  });
