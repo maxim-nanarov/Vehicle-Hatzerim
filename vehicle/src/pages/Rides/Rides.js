c0d4f584eb30c62de38a55cf4e6aac1ebfc7d361
student <you@example.com>
2022-09-21 16:03:39 +0300

    added WIP table to rides

--- vehicle/src/pages/Rides/Rides.js
+++ vehicle/src/pages/Rides/Rides.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./Rides.scss";
 //ToDo: table show rides with filters
 export default function Rides() {
   const [Vehicles, setVehicles] = useState([]);
@@ -11,6 +12,31 @@ export default function Rides() {
         setVehicles(res.data);
       });
   }, []);
+
+  let a = Vehicles.map((veh) => {
+    let count = 0;
+    return (
+      <>
+        <tr className="Card" key={count}>
+          <th>{veh.vehicle_plate_num}</th>
+          <th>{veh.type_id}</th>
+          <th>{veh.vehicle_plate_num}</th>
+        </tr>
+      </>
+    );
+  });
+
   console.log(Vehicles);
-  return <div>Rides</div>;
+  return (
+    <div>
+      <div className="RidesDiv">
+        <tr className="Card">
+          <th>vehicle plate number</th>
+          <th>type id</th>
+          <th>veh.vehicle_plate_num</th>
+        </tr>
+        {a}
+      </div>
+    </div>
+  );
 }
