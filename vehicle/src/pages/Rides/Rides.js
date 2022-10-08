cebaf8f0184315ddc4ea018ff7ebebcb5bbe1464
student <you@example.com>
2022-10-09 00:42:55 +0300

    added a change to the server and applied it to the page

--- vehicle/src/pages/Rides/Rides.js
+++ vehicle/src/pages/Rides/Rides.js
@@ -4,8 +4,16 @@ import "./Rides.scss";
 //ToDo: table show rides with filters
 export default function Rides() {
   const [Vehicles, setVehicles] = useState([]);
+  const [VehiclesData, setVehiclesData] = useState([]);
 
   useEffect(() => {
+    axios
+      .get("http://localhost:4002/Vehicles_And_Its_Relevent_Element")
+      .then(function (res) {
+        setVehiclesData(res.data);
+        console.log(res.data);
+      });
+
     axios
       .get("https://vehicle-hatzerim.herokuapp.com/vehicles")
       .then(function (res) {
@@ -13,18 +21,21 @@ export default function Rides() {
       });
   }, []);
 
-  let a = Vehicles.map((veh) => {
+  let a = VehiclesData.map((veh) => {
     let count = 0;
     return (
       <>
         <tr className="Card" key={count}>
           <th>{veh.vehicle_plate_num}</th>
-          <th>{veh.type_id}</th>
-          <th>{veh.vehicle_plate_num}</th>
+          <th>{veh.company_name}</th>
+          <th>{veh.size_name + ":" + veh.size_capacity}</th>
+          <th>{veh.type_name}</th>
         </tr>
       </>
     );
   });
+
+  console.log(VehiclesData);
 
   console.log(Vehicles);
   return (
@@ -33,8 +44,9 @@ export default function Rides() {
         <table>
           <tr className="Card">
             <th>vehicle plate number</th>
-            <th>type id</th>
-            <th>veh.vehicle_plate_num</th>
+            <th>Company</th>
+            <th>Size Capacity</th>
+            <th>Type</th>
           </tr>
           {a}
         </table>
