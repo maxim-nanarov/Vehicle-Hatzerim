f08db8e5e550aaf0b93a71f1732b6ea089c35977
student <you@example.com>
2022-10-20 22:45:04 +0300

    added ride by date showing in the ride table

--- vehicle/src/pages/Vehicle_Schedule/Vehicle_Schedule.js
+++ vehicle/src/pages/Vehicle_Schedule/Vehicle_Schedule.js
@@ -6,7 +6,6 @@ import "./Vehicle_Schedule.scss";
 export default function VehicleSchedule() {
   const [Rides, setRides] = useState([]);
   const [Data, setData] = useState([]);
-
   useEffect(() => {
     axios
       .get("https://vehicle-hatzerim.herokuapp.com/Rides", {
@@ -30,26 +29,36 @@ export default function VehicleSchedule() {
         setData(res.data);
       });
   }, []);
-  console.log(Data);
   let count = 0;
+  let date = new Date().toISOString().split("T")[0];
+  console.log(date);
   let a = Data.map((Ride) => {
     count++;
+    let sDate = Ride.starting_date.split("T");
+    let fDate = Ride.finishing_date.split("T");
+    if (sDate[0] === date || fDate[0] === date) {
+      console.log("welp that worked");
 
-    return (
-      <tr className="Card" key={count}>
-        <th>{Ride.ride_id}</th>
-        <th>{Ride.vehicle_plate_num}</th>
-        <th>{Ride.destination_name}</th>
-        <th>{Ride.reason_name}</th>
-        <th>
-          {Ride.company_name + " | " + Ride.size_name + " | " + Ride.type_name}
-        </th>
-        <th>{Ride.user_name + "  " + Ride.user_surname} </th>
-        <th>{Ride.will_take_riders.toString()} </th>
-        <th>{prettyDate2(Ride.starting_date)} </th>
-        <th>{prettyDate2(Ride.finishing_date)} </th>
-      </tr>
-    );
+      return (
+        <tr className="Card" key={count}>
+          <th>{Ride.ride_id}</th>
+          <th>{Ride.vehicle_plate_num}</th>
+          <th>{Ride.destination_name}</th>
+          <th>{Ride.reason_name}</th>
+          <th>
+            {Ride.company_name +
+              " | " +
+              Ride.size_name +
+              " | " +
+              Ride.type_name}
+          </th>
+          <th>{Ride.user_name + "  " + Ride.user_surname} </th>
+          <th>{Ride.will_take_riders.toString()} </th>
+          <th>{prettyDate2(Ride.starting_date)} </th>
+          <th>{prettyDate2(Ride.finishing_date)} </th>
+        </tr>
+      );
+    }
   });
   return (
     <div className="MainDivRides">
