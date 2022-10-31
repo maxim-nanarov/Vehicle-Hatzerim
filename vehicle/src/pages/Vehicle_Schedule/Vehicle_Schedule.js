c04e5f64d428f604a3fb34b9fd02f3b290becf88
student <you@example.com>
2022-10-31 11:43:54 +0200

    the filters work individually

--- vehicle/src/pages/Vehicle_Schedule/Vehicle_Schedule.js
+++ vehicle/src/pages/Vehicle_Schedule/Vehicle_Schedule.js
@@ -9,8 +9,8 @@ import "react-date-range/dist/theme/default.css"; // theme css file
 export default function VehicleSchedule() {
   const [Rides, setRides] = useState([]);
   const [DateFilter, setDateFilter] = useState();
-  const [DestinationFilter, setDestinationFilter] = useState();
-  const [plateNum, setPlateNum] = useState();
+  const [DestinationFilter, setDestinationFilter] = useState("");
+  const [plateNum, setPlateNum] = useState("");
   const [Data, setData] = useState([]);
   const [Filter, setFilter] = useState([]);
   useEffect(() => {
@@ -38,16 +38,16 @@ export default function VehicleSchedule() {
       });
   }, []);
   useEffect(() => {
-    console.log(plateNum, DestinationFilter);
     //Filter
-    if (plateNum !== undefined) {
+    if (plateNum !== "") {
       setFilter(FilterplateNum(Data, Filter, plateNum));
-    }
-    if (DestinationFilter !== undefined) {
+    } else if (DestinationFilter !== "") {
       setFilter(FilterDestination(Data, Filter, DestinationFilter));
+    } else {
+      setFilter([]);
     }
-    console.log(Filter);
-  }, [plateNum, DestinationFilter, Filter]);
+  }, [plateNum, DestinationFilter]);
+  console.log(plateNum);
   let count = 0;
   let date;
   if (DateFilter !== undefined) {
@@ -55,16 +55,13 @@ export default function VehicleSchedule() {
   } else {
     date = new Date().toLocaleString().split(",")[0];
   }
-  let a = Data.map((Ride) => {
-    count++;
-    let sDate = new Date(Ride.starting_date).toLocaleString().split(",")[0];
-    let fDate = new Date(Ride.finishing_date).toLocaleString().split(",")[0];
-    if (sDate === date || fDate === date) {
-      if (
-        plateNum === undefined ||
-        DestinationFilter === undefined ||
-        plateNum === ""
-      ) {
+  let a;
+  if (Filter.length > 0) {
+    a = Filter.map((Ride) => {
+      count++;
+      let sDate = new Date(Ride.starting_date).toLocaleString().split(",")[0];
+      let fDate = new Date(Ride.finishing_date).toLocaleString().split(",")[0];
+      if (sDate === date || fDate === date) {
         return (
           <tr className="Card" key={count}>
             <th>{Ride.ride_id}</th>
@@ -84,36 +81,41 @@ export default function VehicleSchedule() {
             <th>{prettyDate2(Ride.finishing_date)} </th>
           </tr>
         );
-      } else if (
-        plateNum === Ride.vehicle_plate_num &&
-        Ride.destination_name.indexOf(DestinationFilter) > -1
-      ) {
-        console.log(plateNum, DestinationFilter);
-
-        return (
-          <tr className="Card" key={count}>
-            <th>{Ride.ride_id}</th>
-            <th>{Ride.vehicle_plate_num}</th>
-            <th>{Ride.destination_name}</th>
-            <th>{Ride.reason_name}</th>
-            <th>
-              {Ride.company_name +
-                " | " +
-                Ride.size_name +
-                " | " +
-                Ride.type_name}
-            </th>
-            <th>{Ride.user_name + "  " + Ride.user_surname} </th>
-            <th>{Ride.will_take_riders.toString()} </th>
-            <th>{prettyDate2(Ride.starting_date)} </th>
-            <th>{prettyDate2(Ride.finishing_date)} </th>
-          </tr>
-        );
+      } else {
+        return <div></div>;
       }
-    } else {
-      return <div></div>;
-    }
-  });
+    });
+  } else {
+    a = Data.map((Ride) => {
+      count++;
+      let sDate = new Date(Ride.starting_date).toLocaleString().split(",")[0];
+      let fDate = new Date(Ride.finishing_date).toLocaleString().split(",")[0];
+      if (sDate === date || fDate === date) {
+        return (
+          <tr className="Card" key={count}>
+            <th>{Ride.ride_id}</th>
+            <th>{Ride.vehicle_plate_num}</th>
+            <th>{Ride.destination_name}</th>
+            <th>{Ride.reason_name}</th>
+            <th>
+              {Ride.company_name +
+                " | " +
+                Ride.size_name +
+                " | " +
+                Ride.type_name}
+            </th>
+            <th>{Ride.user_name + "  " + Ride.user_surname} </th>
+            <th>{Ride.will_take_riders.toString()} </th>
+            <th>{prettyDate2(Ride.starting_date)} </th>
+            <th>{prettyDate2(Ride.finishing_date)} </th>
+          </tr>
+        );
+      } else {
+        return <div></div>;
+      }
+    });
+  }
+
   return (
     <div className="MainDivRides">
       <div className="Headline">
@@ -203,38 +205,61 @@ function prettyDate2(time) {
 
 //To Do: Filters functions that seperated from the main code.
 function FilterplateNum(Data, Filter, plateNum) {
-  if (Filter !== undefined) {
-    Filter = Filter.map((Ride) => {
-      if (Ride.vehicle_plate_num === plateNum) {
-        return Ride;
+  let Result = [];
+  if (Filter.length > 0) {
+    for (let i = 0; i < Filter.length; i++) {
+      if (Filter[i].vehicle_plate_num === plateNum) {
+        Result.push(Filter[i]);
       }
-      return undefined;
-    });
+      console.log("Vehicle Schedual: " + Filter[i]);
+    }
   } else {
-    Filter = Data.map((Ride) => {
-      if (Ride.vehicle_plate_num === plateNum) {
-        return Ride;
+    for (let i = 0; i < Data.length; i++) {
+      if (Data[i].vehicle_plate_num === Number(plateNum)) {
+        Result.push(Data[i]);
+        console.log(
+          Data[i].vehicle_plate_num === Number(plateNum),
+          Data[i].vehicle_plate_num,
+          Number(plateNum)
+        );
       }
-      return undefined;
-    });
+      console.log("Vehicle Schedual: " + Data[i]);
+    }
   }
+  if (Result.length > 0) {
+    console.log(Result);
+  }
+  console.log("vehicle plate num: " + plateNum);
+  return Result;
 }
 
 function FilterDestination(Data, Filter, Destination) {
-  console.log(Data);
-  if (Filter !== undefined) {
-    Filter = Filter.map((Ride) => {
-      if (Ride.destination_name === Destination) {
-        return Ride;
+  let Result = [];
+  if (Filter.length > 0) {
+    console.log(Destination);
+    for (let i = 0; i < Filter.length; i++) {
+      console.log(
+        Filter[i].destination_name.includes(Destination),
+        Filter[i].destination_name,
+        Destination
+      );
+      if (Filter[i].destination_name.includes(Destination)) {
+        Result.push(Filter[i]);
       }
-      return undefined;
-    });
+    }
   } else {
-    Filter = Data.map((Ride) => {
-      if (Ride.destination_name === Destination) {
-        return Ride;
+    console.log(Destination);
+    for (let i = 0; i < Data.length; i++) {
+      console.log(
+        Data[i].destination_name.includes(Destination),
+        Data[i].destination_name,
+        Destination
+      );
+      if (Data[i].destination_name.includes(Destination)) {
+        Result.push(Data[i]);
       }
-      return undefined;
-    });
+    }
   }
+  console.log(Data[0].destination_name);
+  return Result;
 }
